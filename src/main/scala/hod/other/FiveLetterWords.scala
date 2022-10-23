caae21e04c15e21415079f638d77f29d299d11e9
hamsterofdeath <h-star@gmx.de>
2022-10-23 21:19:31 +0200

    ...

--- src/main/scala/hod/other/FiveLetterWords.scala
+++ src/main/scala/hod/other/FiveLetterWords.scala
@@ -8,7 +8,7 @@ import java.util.concurrent.atomic.AtomicInteger
 import scala.collection.parallel.CollectionConverters.ArrayIsParallelizable
 import scala.util.Random
 
-import hod.euler.measured
+import hod.euler.{bench, measured}
 import java.util.concurrent.{ExecutorService, Executors, TimeUnit}
 import scala.collection.Searching
 
@@ -37,110 +37,26 @@ object FiveLetterWords {
   def charToArrayIndex(c: Char) = c - aCode
   def arrayIndexToChar(c: Int): Char = (c + aCode).toChar
 
-  val allWords = String(
-    Files.readAllBytes(
-      Paths.get("resource/words_alpha.txt")),
-    StandardCharsets.UTF_8)
-    .linesIterator
-    .filter(e => Integer.bitCount(wordToCode(e)) == 5)
-    .filter(_.length == 5)
-    .toArray
-
-  private class Node(val letterBit: Int, val depth: Int) {
-    private      val ownLetter = if (depth > 0) bitToLetter(letterBit) else '?'
-    private var isWord         = false
-    private var hasWords       = false
-    private lazy val nodes     = Array
-      .tabulate[Node](26)(e => Node(letterToBit(arrayIndexToChar(e)), depth + 1))
-    def insert(word: String): Unit = {
-      if (depth < 5) {
-        hasWords = true
-        val charToCheck = charToArrayIndex(word(depth))
-        nodes(charToCheck).insert(word)
-      } else {
-        isWord = true
-      }
-    }
-
-    def foreachExcept[U](code: Int, atLeast: Array[Char])(cb: ((Array[Char], Int) => U)): Unit = {
-      foreachExcept(code, Array.tabulate[Char](5)(_ => ' '), 0, atLeast)(cb)
-    }
-
-    private def foreachExcept[U](code: Int, hack: Array[Char], hackCode: Int, atLeast: Array[Char])
-                                (cb: (Array[Char], Int) => U): Unit = {
-      if (depth > 0) {
-        hack(depth - 1) = ownLetter
-      }
-
-      if (hasWords) {
-        nodes.foreach { subNode =>
-          def isAtLeast = {
-            util.Arrays.compare(hack, 0, depth, atLeast, 0, depth) >= 0
-          }
-
-          if ((subNode.letterBit & code) == 0 && isAtLeast) {
-            subNode.foreachExcept(code, hack, hackCode | letterBit, atLeast)(cb)
-          }
-        }
-      } else if (isWord) {
-        cb(hack, hackCode | letterBit)
-      }
-    }
-
-    override def toString = s"$ownLetter"
-  }
-
-  def v1(): Unit = {
-    val root = Node(0, 0)
-
-    allWords.foreach { word =>
-      root.insert(word)
-    }
-
-    println(s"Checking ${allWords.length} words")
-    val counter  = AtomicInteger(0)
-    val progress = AtomicInteger(0)
-    measured {
-      val service = Executors.newFixedThreadPool(30)
-      allWords.foreach { firstWord =>
-        service.submit(new Runnable {
-          override def run(): Unit = {
-            if (progress.incrementAndGet() % 100 == 0) {
-              print('.')
-            }
-            val level1 = wordToCode(firstWord)
-            root.foreachExcept(level1, firstWord.toCharArray) { (secondWord, level2) =>
-              val level12 = level1 | level2
-              root.foreachExcept(level12, secondWord) { (thirdWord, level3) =>
-                val level123 = level12 | level3
-                root.foreachExcept(level123, thirdWord) { (forthWord, level4) =>
-                  val level1234 = level123 | level4
-                  root.foreachExcept(level1234, forthWord) { (fifthWord, _) =>
-                    counter.incrementAndGet()
-                    //   println(s"$w1,$w2,$w3,$w4,$w5")
-                  }
-                }
-              }
-            }
-          }
-        })
-      }
-      service.shutdown()
-      service.awaitTermination(Long.MaxValue, TimeUnit.DAYS)
-    }
-    println("Done")
-    println(counter.get())
-
-  }
-
   def v2(): Unit = {
-    measured {
-      val count = AtomicInteger()
+    bench("Total") {
+      val service = Executors.newFixedThreadPool(30)
+      val count   = AtomicInteger()
+
+      val allWords = bench("Load words") {
+        String(
+          Files.readAllBytes(
+            Paths.get("resource/words_alpha.txt")),
+          StandardCharsets.UTF_8)
+          .linesIterator
+          .filter(e => Integer.bitCount(wordToCode(e)) == 5)
+          .filter(_.length == 5)
+          .toArray
+      }
 
       def takeAfter(word: BitWord, words: IndexedSeq[BitWord]) = {
         words.search(word) match
           case Searching.Found(foundIndex) => words.drop(foundIndex)
-          case Searching.InsertionPoint(insertionPoint) => words.drop(insertionPoint - 1)
+          case Searching.InsertionPoint(insertionPoint) => words.drop(insertionPoint)
       }
 
       case class BitWord(word: String, bits: Int) extends Comparable[BitWord] {
@@ -148,61 +64,61 @@ object FiveLetterWords {
         override def compareTo(o: BitWord): Int = word.compareTo(o.word)
       }
 
-      class WorkingSet(words: List[BitWord], code: Int, wordPool: IndexedSeq[BitWord], depth: Int) {
+      class WorkingSet(words: List[BitWord], code: Int, val wordPool: List[BitWord], depth: Int) {
+
+        def recur(): Unit = {
+          if (stepsLeft > 0) {
+            forkEach(_.recur())
+          } else {
+            count.incrementAndGet()
+            println(currentSolution)
+          }
+        }
 
         def stepsLeft = 5 - depth
 
-        def forked(nextWord: BitWord) = {
-          WorkingSet(
-            nextWord :: words,
-            code | nextWord.bits,
-            takeAfter(nextWord, wordPool)
-              .filterNot(_.overlapsWith(nextWord)),
-            depth + 1
-          )
-        }
+        def forkEach(cb: WorkingSet => Unit) = {
+          if (wordPool.nonEmpty) {
+            wordPool.tails.foreach { tryWords =>
+              if (tryWords.nonEmpty) {
+                def doWork(): Unit = {
+                  val nextWord = tryWords.head
+                  val pool     = tryWords.filterNot(_.overlapsWith(nextWord))
+                  cb(WorkingSet(
+                    nextWord :: words,
+                    code | nextWord.bits,
+                    pool,
+                    depth + 1
+                  ))
+                }
 
-        def nextLevelCandidates = {
-          wordPool
+                if (depth == 0) {
+                  service.submit(new Runnable {
+                    override def run(): Unit = doWork()
+                  })
+                } else {
+                  doWork()
+                }
+              }
+            }
+          }
         }
 
         def currentSolution = words.reverse.map(_.word)
+        override def toString = s"WorkingSet($stepsLeft, $currentSolution)"
       }
 
-      val words = allWords.map(w => BitWord(w, wordToCode(w)))
-      val graph = words.map { bw => bw -> words.filterNot(_.overlapsWith(bw)).toIndexedSeq }.toMap
+      val words = bench("Bit fun") {allWords.map(w => BitWord(w, wordToCode(w)))}
 
+      WorkingSet(
+        Nil,
+        0,
+        words.toList,
+        0).recur()
 
-      def recur(state: WorkingSet): Unit = {
-        val stepsLeft = state.stepsLeft
-        if (stepsLeft > 0) {
-          state.nextLevelCandidates.foreach { nextWord =>
-            recur(state.forked(nextWord))
-          }
-        } else {
-          count.incrementAndGet()
-          println(state.currentSolution)
-        }
-      }
-
-      val service = Executors.newFixedThreadPool(30)
-      words.foreach { start =>
-        service.submit(new Runnable {
-          override def run(): Unit = {
-            val helper = WorkingSet(
-              List(start),
-              start.bits,
-              takeAfter(start, graph(start)),
-              1)
-            recur(helper)
-          }
-        })
-      }
       service.shutdown()
       service.awaitTermination(Long.MaxValue, TimeUnit.DAYS)
-      println(count.get())
     }
-
   }
 
   def main(args: Array[String]): Unit = {
